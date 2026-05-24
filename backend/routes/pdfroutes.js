84305f43089163e5e2c773f1d368c42773ae48ad
ihsansani <ytchannel0666@gmail.com>
2026-05-24 18:07:45 +0500

    fix pdfController variable name

--- backend/routes/pdfroutes.js
+++ backend/routes/pdfroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 
-const pdfcontroller = require("../controllers/pdfcontroller");
+const pdfController = require("../controllers/pdfcontroller");
 
 // storage
 const storage = multer.diskStorage({
